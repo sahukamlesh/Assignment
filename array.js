5db008fe2da0e25ef3670f0d1802ba634a869caa
sahukamlesh <67776233+sahukamlesh@users.noreply.github.com>
2020-10-17 21:36:51 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+function rohit(n,ar){
+let socks = {};
+let  = 0;
+for (let elem of ar) {
+socks[elem] = socks[elem] + 1 || 1;
+  if (socks[elem] % 2 == 0) {
+    pairs += 1;
+  }
+}
+return pairs;
+}
+
+
+
